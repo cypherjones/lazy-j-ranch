a068cedfff70623240822e8ee14ce2cf0db4fb73
cypherjones <aj@heuristixdev.com>
2015-06-27 13:16:13 -0500

    updated front page

--- wp-content/themes/lazyj/front-page.php
+++ wp-content/themes/lazyj/front-page.php
@@ -1,29 +1,7 @@
 <?php get_header( ); ?>
-<div id="main-slider" class="m-slider">
-<?php 
-  
-  if (have_rows('fp_slider_elements')) : 
-    while (have_rows('fp_slider_elements')) : 
-      the_row(); 
-?>
-
-  <section class="slider-background" style="background-image: url('<?php the_sub_field('fp_slider_image') ?>');">
-    <div class="main-slider-content-box">
-      <div class="main-slider-title">
-        <?php the_sub_field('fp_slider_title_text') ?>
-      </div>
-      <div class="main-slider-cta-box btn">
-        <a href="<?php the_sub_field('fp_slider_cta_link') ?>">
-          <?php the_sub_field('fp_slider_cta_text') ?> <i class="fa fa-angle-right"></i>
-        </a>
-      </div>
-    </div>
-  </section>
-
-<?php endwhile; endif; ?>
-</div>
-<section id="wood-divider" class="divider-background" style="background-image: url('<?php bloginfo('template_directory' ); ?>/img/wood_bar_divider.png')">
-</section>
+<!-- ============ slider ============ -->
+<?php get_template_part('content/section', 'slider' ); ?>
+<!-- ============ content ============ -->
 <section id="content">
   <div class="promo-box">
     <div class="container">
